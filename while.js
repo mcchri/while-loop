46dde7e8ed688d306ff54ad17a20a4bd7b250677
mcchri <112865223+mcchri@users.noreply.github.com>
2022-10-10 11:54:35 +0100

    Add files via upload

---
+++ while.js
@@ -0,0 +1,16 @@
+//while loop execute a statement 
+
+let count = 0;
+while(count<10){
+    console.log(count);
+    count++; //count = count+1
+}
+
+//Do while loop 
+//Always runs the code
+//at least ounce
+do{
+    console.log(count);
+    count++;
+}while(count<20);
+
